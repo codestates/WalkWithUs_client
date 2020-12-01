02eea69578ff7144c1fe5ba79c9361d61e013c2d
DavidBak <sejongbgy07@gmail.com>
2020-12-01 23:11:38 +0900

    [Client]#13 main 페이지 구현

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,15 +1,38 @@
 import React,{Component} from "react";
 import PropTypes from "prop-types";
-
+import {Link} from "react-router-dom"
 import axios from "axios";
 import "./Main.css"
 
 
 class Main extends Component {
+    constructor(props){
+        super(props)
+        this.state = {};
+    }
     render() {
         return (
             <div>
-            Main
+             
+             <div className="videoListBody">
+              <Link to={`/video/videolist`}>
+              <button>walk with us</button>
+              </Link>
+              <Link to={`/video/uploadvideo`}>
+              <div>Upload your video here</div>
+              </Link>
+             </div>
+
+             <div className="videoListFooter">
+              <Link to ={{pathname: "https://www.instagram.com/walkwithus3/"}} target="_blank">
+                <div>Instagram</div>
+              </Link>  
+              <Link to ={{pathname: "https://www.buymeacoffee.com/home"}} target="_blank">
+                <div>Support us</div>
+              </Link>  
+              
+             </div>
+
             </div>
         );
     }
