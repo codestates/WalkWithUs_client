d7a7b5b4c3eeac1e813387f75992c9776088891a
DavidBak <sejongbgy07@gmail.com>
2020-12-06 23:31:35 +0900

    [Client]#feat Sign up 페이지 포스트 요청 구현

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -3,7 +3,7 @@ import PropTypes from "prop-types";
 import "./SignUp.css"
 import axios from "axios";
 
-
+const IP_ADDRESS = "";
 
 class SignUp extends Component {
     constructor(props){
@@ -19,7 +19,7 @@ class SignUp extends Component {
         this.setState({[key]: e.target.value})
     }
     render() {
-        const {email, password, nickName} = this.state;
+        const {email, password, username} = this.state;
         const {isLogin, userInfo} = this.props;
         if(!isLogin){
             return (
@@ -28,7 +28,25 @@ class SignUp extends Component {
                     <h1>Sign up</h1>
                     <br/>
                     <br/>
-                    <form>
+                    <form
+                       onSubmit={(e) => {
+                        e.preventDefault();
+                        if (email && password && username) {
+                          axios
+                            .post(`http://${IP_ADDRESS}:3001/user/signup`, {
+                              email: email,
+                              password: password,
+                              nickName: username,
+                            })
+                            .then(() => {
+                              this.props.history.push(`/`);
+                            })
+                            .catch((err) => console.log(err));
+                        } else {
+                          alert("가입이 완료되지 않았습니다.");
+                        }
+                      }}
+                    >
                        <input
                        type="email"
                        placeholder="What is your email?"
@@ -47,9 +65,6 @@ class SignUp extends Component {
                        <button type="submit">Submit</button>
 
                        <h2>welcome to walk with us!</h2>
-
-
-
                     </form>
 
                 </center>
