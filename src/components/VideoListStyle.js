f60a697e2b117c1e34758115a8c07a2b69a2611d
DavidBak <sejongbgy07@gmail.com>
2020-12-14 01:05:35 +0900

    [Client]#feat35 VideoList 페이지 CSS 완성

---
+++ src/components/VideoListStyle.js
@@ -0,0 +1,21 @@
+import { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
+
+import reset from "styled-reset";
+
+const GlobalStyle = createGlobalStyle`
+    ${reset};
+   
+   
+    body{
+       
+        font-size: 1rem;
+		color:#fff;
+        width:100%;
+        height:100%;
+        background: black;
+        background-size:cover;
+    }
+`;
+
+export {GlobalStyle}
