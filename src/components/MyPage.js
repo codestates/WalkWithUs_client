3a8826cc6b6fb1cce8c9eb4b649393f194e390bc
Jihee0308 <yjhe333@gmail.com>
2020-12-14 18:53:40 +0900

    [Client] #80 fix mypage and mypage css

--- src/components/MyPage.js
+++ src/components/MyPage.js
@@ -2,15 +2,13 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./MyPage.css";
 import axios from "axios";
-
+import TextField from "@material-ui/core/TextField";
 import { withRouter } from "react-router-dom";
+import backgroundimg from "../image/backgroundimg.jpg";
 
 const IP_ADDRESS = "127.0.0.1";
 
-
-
 // const IP_ADDRESS = "52.78.59.129";
-
 
 const axiosInstance = axios.create({
   withCredentials: true,
@@ -40,70 +38,73 @@ class MyPage extends Component {
     }
   }
 
-
-    handleInputValue = (key) => (e) => {
-        this.setState({ [key]: e.target.value });
-      };
-    render() {
-        const {email, username, password, socialinfo} = this.state
-        const {isLogin, userInfo, handleIsLoginChange} = this.props;
-        if(isLogin){
-            return (
-                <div className="mypageContainer">
-                    <h1>My page</h1>
-                    <form
-                     onSubmit={(e) => {
-                        e.preventDefault();
-                        return axios
-                          .put(`http://${IP_ADDRESS}:3001/user/useredit`, {
-                          email:email,
-                          username:username,
-                          password:password,
-                          socialinfo:socialinfo
-                          })
-                          .then((res) => {
-                           
-                            handleIsLoginChange(JSON.parse(res.config.data))
-                            alert("회원정보가 수정되었습니다")
-                          })
-                          .catch((err) => {
-                            alert("비밀번호를 확인해 주세요");
-                            console.log(err);
-                          });
-                      }}
-                    >
-
-            <div className="inputText">
-              <div className="email">
-                <div className="email">{email}</div>
+  handleInputValue = (key) => (e) => {
+    this.setState({ [key]: e.target.value });
+  };
+  render() {
+    const { email, username, password, socialinfo } = this.state;
+    const { isLogin, userInfo, handleIsLoginChange } = this.props;
+    if (isLogin) {
+      return (
+        <div className="mypageContainer">
+          <div>
+            <img className="backgroundimg" src={backgroundimg}></img>
+          </div>
+          <h1 className="mypage">My page</h1>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              return axios
+                .put(`http://${IP_ADDRESS}:3001/user/useredit`, {
+                  email: email,
+                  username: username,
+                  password: password,
+                  socialinfo: socialinfo,
+                })
+                .then((res) => {
+                  handleIsLoginChange(JSON.parse(res.config.data));
+                  alert("회원정보가 수정되었습니다");
+                })
+                .catch((err) => {
+                  alert("비밀번호를 확인해 주세요");
+                  console.log(err);
+                });
+            }}
+          >
+            <div className="textall">
+              <div className="">
+                <TextField className="email" value={`${email}`}></TextField>
               </div>
-              <div className="input">
-                <input
+              <div>
+                <TextField
+                  className="username"
                   type="username"
                   value={`${username}`}
-                  placeholder="enter your username"
+                  placeholder="닉네임을 입력하세요"
                   onChange={this.handleInputValue("username")}
-                ></input>
+                ></TextField>
               </div>
 
-              <div className="input">
-                <input
+              <div>
+                <TextField
+                  className="socialinfo"
                   type="text"
                   value={socialinfo ? socialinfo : ``}
-                  placeholder="enter your social info"
+                  placeholder="소셜 정보를 입력하세요"
                   onChange={this.handleInputValue("socialinfo")}
-                ></input>
+                ></TextField>
               </div>
-              <div className="input">
-                <input
+              <div>
+                <TextField
+                  className="password"
                   type="password"
-                  placeholder="enter your own password"
+                  placeholder="비밀번호를 입력하세요"
                   onChange={this.handleInputValue("password")}
-                ></input>
+                ></TextField>
               </div>
             </div>
 
-            <button type="submit" className="edit">
+            <button type="submit" className="editbtn">
               Edit
             </button>
           </form>
