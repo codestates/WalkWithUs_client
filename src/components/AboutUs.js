fa7cb236ee7c640b3658f5334e763b84a5b2fbef
Jihee0308 <yjhe333@gmail.com>
2020-12-18 01:01:40 +0900

    [Client]#32 modify about us css & js file  for css

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -9,7 +9,14 @@ import ShareStory from "./shareYourStory";
 import logo from "../image/walkLogoBlack.png";
 import walkingtext from "../image/walkingtext.png";
 import walkingtext2 from "../image/walkingtext2.png";
+import walkingtext3 from "../image/walkingtext3.png";
+import walkingtext4 from "../image/walkingtext4.png";
 
+import image1 from "../image/image1.jpg";
+import instagramlogo from "../image/instagramlogo.jpg";
+import video from "../video/share.mp4";
+import arrow from "../image/arrow.jpg";
+import testimg from "../image/testimg.png";
 class AboutUs extends Component {
   constructor(props) {
     super(props);
@@ -27,54 +34,77 @@ class AboutUs extends Component {
                 </Link>
               </header>
             </div>
-            <img className="walkingtext2" src={walkingtext2} />
+            <div>
+              <span>
+                <img className="walkingtext" src={walkingtext} />
+              </span>
+              <span>
+                <img className="walkingtext2" src={walkingtext2} />
+              </span>
+              <span>
+                <img className="walkingtext3" src={walkingtext3} />
+              </span>
+              <span>
+                <img className="walkingtext4" src={walkingtext4} />
+              </span>
+            </div>
 
-            <img className="walkingtext" src={walkingtext} />
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-            <br></br>
-
-            <br></br>
             <h1 className="ourVision">Our vision</h1>
             <p className="ourvision2">
-              언젠가부터, 우리에게 너무나도 당연했던 산책이 힘든 세상이
-              되었습니다.
+              우리에게 너무나도 당연했던 외출이 힘든 세상이 되었습니다.
               <br />
-              이로 인한, 우울감을 WalkWith Us를 통해 각자의 산책을 공유하면서,
-              조금이나마 마음의 답답함을 푸실 수 있기를 바랍니다.
+              저희 Walk with us를 통해 여러분들의 걷는 모습을 공유하고,
               <br />
-              저희 서비스가 사람들에게 작은 희망이 될 수 있기를 기원합니다.{" "}
+              저희 서비스를 통해 모두가 힐링하는 하루가 되기를 소망합니다.
             </p>
+            <div className="corona">
+              *코로나 사태가 심각해지고 있습니다. 저희 walkwithus는 외출 권장이
+              아닌, 추억을 함께 공유하는 공간입니다*
+            </div>
           </div>
-
           <div className="page2"></div>
-
           <div className="page3">
-            <h1>Share your story</h1>
-
-            <ShareStory />
-          </div>
-
-          <div className="page4">
-            <h1>Let me introduce our team member!</h1>
             <br></br>
+            <h1 className="shareyourstory">SHARE YOUR STORY</h1>{" "}
+            <span>
+              <Link
+                to={{ pathname: "https://www.instagram.com/walkwithus3/" }}
+                target="_blank"
+              >
+                <img className="instagramlogo" src={instagramlogo} />
+              </Link>
+            </span>
+            <video
+              className="swissvideo"
+              autoPlay="autoplay"
+              muted="muted"
+              loop
+            >
+              <source src={video} type="video/mp4" />
+            </video>
+            <div className="page1">
+              <img className="image1" src={image1} />
+            </div>{" "}
+            <div className="wrap">
+              <img className="arrow" src={arrow} />
+            </div>
           </div>
-
-          <Introduce />
+          <div className="page4">
+            <h1 className="introduction">INTRODUCE OUR TEAM MEMBER</h1>
+            <br></br>
+            <br></br>
+            <br></br>{" "}
+            <div className="testimg" width="1%">
+              <img src={testimg} />
+            </div>
+            <Introduce />
+          </div>
         </div>
       </div>
     );
   }
 }
 {
-  /* <Link to ={{pathname: "https://www.instagram.com/walkwithus3/"}} target="_blank">
-<span className="pink">#우울함은 이제 안녕</span>
-</Link> */
 }
 
 export default withRouter(AboutUs);
--- src/components/introduce.js
+++ src/components/introduce.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import jihee from "../image/jihee.png"
-import juyeon from "../image/juyeon.png"
-import gwonyong from "../image/gwonyong.png"
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Paper from "@material-ui/core/Paper";
+import Grid from "@material-ui/core/Grid";
+import jihee from "../image/jihee.png";
+import juyeon from "../image/juyeon.png";
+import gwonyong from "../image/gwonyong.png";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
   },
   paper: {
-    padding: theme.spacing(1),
-    textAlign: 'center',
+    padding: theme.spacing(2),
+    textAlign: "center",
     color: theme.palette.text.secondary,
   },
 }));
@@ -24,52 +23,56 @@ export default function NestedGrid() {
   function FormRow() {
     return (
       <React.Fragment>
-        <Grid item xs={4}>
+        <Grid className="grid" item xs={3}>
           <Paper className={classes.paper}>
-            <img src={juyeon} width="200px"/>
-            <p>
-              Juyeon Lee <br/><br/>
-            
-              &middot; Back-end <br/>
-              &middot; Email:  ju71711@gmail.com <br/>
-              <br/>
-              안녕하세요. 이주연입니다.<br/>
-              저는 --한 개발자가 되고 싶습니다.<br/>
-              감사합니다.
-              
+            <img className="juyeon" src={juyeon} width="150px" />
+            <p className="juyeonname">
+              Juyeon Lee <br />
+              <br />
+              &middot; Back-end <br />
+              &middot; Email: ju71711@gmail.com <br />
+              <br />
+              안녕하세요 이주연입니다.
+              <br />
+              저는 재미난 일상을 만드는
+              <br />
+              개발자가 되고 싶습니다.
             </p>
           </Paper>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
           <Paper className={classes.paper}>
-            <img src={gwonyong} width="200px"/>
-            <p>
-            Gwonyong Park <br/><br/>
-            
-              &middot; Front-end, Team leader <br/>
-              &middot; Email: sejongbgy07@gmail.com<br/>
-              <br/>
-              안녕하세요 박권용 입니다.<br/>
-저의 기술로 세상에 조금이나마<br/>
-도움이 되고 싶습니다.
+            <img className="gwonyong" src={gwonyong} width="150px" />
+            <p className="gwonyongname">
+              Gwonyong Park <br />
+              <br />
+              &middot; Front-end, Team leader <br />
+              &middot; Email: sejongbgy07@gmail.com
+              <br />
+              <br />
+              안녕하세요 박권용 입니다.
+              <br />
+              저의 기술로 세상에 조금이나마
+              <br />
+              도움이 되고 싶습니다.
             </p>
           </Paper>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={3}>
           <Paper className={classes.paper}>
-          <img src={jihee} width="200px"/>
-          <p>
-          Jihee Yoon <br/><br/>
-            
+            <img className="jihee" src={jihee} width="150px" />
+            <p className="jiheename">
+              Jihee Yoon <br />
+              <br />
               &middot; Full-stack, Translate, Design
- <br/>
-              &middot; Email: yjhe333@gmail.com<br/>
-              <br/>
-              안녕하세요 윤지희 입니다.<br/>
-
-저는 사람들을 돕고 성장하는<br/> 개발자가 되고 싶습니다.
-
-
+              <br />
+              &middot; Email: yjhe333@gmail.com
+              <br />
+              <br />
+              안녕하세요 윤지희 입니다.
+              <br />
+              저는 사람들을 돕고 성장하는
+              <br /> 개발자가 되고 싶습니다.
             </p>
           </Paper>
         </Grid>
@@ -79,11 +82,10 @@ export default function NestedGrid() {
 
   return (
     <div className={classes.root}>
-      <Grid container spacing={1}>
-        <Grid container item xs={12} spacing={3}>
+      <Grid container justify="center" spacing={10}>
+        <Grid container justify="center" item xs={20} spacing={9}>
           <FormRow />
         </Grid>
-      
       </Grid>
     </div>
   );
